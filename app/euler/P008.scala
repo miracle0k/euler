44923b6cd93ed622699d9f1d1b3356cc3301d80f
Miracle <miracle@miracle-ui-MacBook-Air.local>
2013-06-02 07:21:30 +0900

    문제 8

---
+++ app/euler/P008.scala
@@ -0,0 +1,15 @@
+package euler
+
+class P008 {
+  def run(seq:Seq[Char], max:Int=Int.MinValue, count:Int=0, product:Int=1): Int = {
+    if(seq.isEmpty) {
+      return max
+    }
+    println(seq.head, max, count, product)
+    if(count < 4) {
+      run(seq.tail, max, count+1, product*seq.head.asDigit)
+    } else {
+      run(seq.tail, product.max(max), 0, 1)
+    }
+  }
+}
