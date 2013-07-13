0a543710c2c888c67eaa86f0f5b4e559e580df77
Miracle <kmiracle@gmail.com>
2013-07-13 20:23:09 +0900

    1414141414141414141414141414

---
+++ app/euler/P014.scala
@@ -0,0 +1,24 @@
+package euler
+
+object P014 extends App {
+  def run(n : Int, cnt: Int = 0) : Int = {
+    n match {
+      case 1 => cnt + 1
+      case number if number % 2 == 0 => run(number/2, cnt + 1)
+      case _ => run(3*n + 1, cnt + 1)
+    }
+  }
+  
+  val range = 1 to 1000000
+  
+  val r = range.foldLeft((1, 0))((b, a) => {
+    val cnt = run(a)
+    if (b._2 < cnt) {
+      (a, cnt)
+    } else {
+      b
+    }
+  })
+  
+  print(r)
+}
