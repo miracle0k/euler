bb408a5dc6c0767e6f38796f791905a53ee71f8a
Miracle <kmiracle@gmail.com>
2013-07-14 11:18:35 +0900

    Int를 이용시 오버플로우가 생길 수 있다. - 방어 코드 추가. - Long으로 변경.

--- app/euler/P014.scala
+++ app/euler/P014.scala
@@ -1,16 +1,17 @@
 package euler
 
 object P014 extends App {
-  def run(n : Int, cnt: Int = 0) : Int = {
+  def run(n : Long, cnt: Int = 0) : Int = {
     n match {
+      case number if number < 1 => throw new IllegalArgumentException("n은 음수가 될 수 없다." + n)
       case 1 => cnt + 1
       case number if number % 2 == 0 => run(number/2, cnt + 1)
       case _ => run(3*n + 1, cnt + 1)
     }
   }
   
-  //val range = 1 to 1000000
-  val range = 1 to 100
+  val range = 1 to 1000000
+  //val range = 1 to 100
 
   val r = range.foldLeft((1, 0))((b, a) => {
     val cnt = run(a)
